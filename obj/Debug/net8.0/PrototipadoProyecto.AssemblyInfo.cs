a71e8e482fa94f33e71d61064a947e7d433bcec9
Julian Villada <juliandavillada1606@gmail.com>
2025-09-06 12:18:02 -0500

    Rama Julian

--- obj/Debug/net8.0/PrototipadoProyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrototipadoProyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrototipadoProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+526919e05cc1bd8bc3d25d4d7e98691621d4912d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da5903967449c86edbe6a4063f209cd3662c2b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrototipadoProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrototipadoProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
