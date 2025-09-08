bf9f365469df4366e5296ef25adf7bb91de436c6
JuanCasZa <juanjose-castro2@hotmail.com>
2025-09-08 11:51:01 -0500

    Todo el código para la creacion de las tablas

--- ArchivosExtra/SQL/dbGimnasios.sql
+++ ArchivosExtra/SQL/dbGimnasios.sql
@@ -19,11 +19,63 @@ create table [Personas]
 
 create table [Clientes]
 (
-	[Id] int primary key identity(1,1),
+	[Id] int primary key references [Personas]([Id]), /*Heredando la misma clave primaria de Personas*/
 	[Estatura] decimal(4,2) not null,
-	[Peso] decimal(5,2) not null,
-	[IdPersona] int references [Personas]([Id]) not null
+	[Peso] decimal(5,2) not null
+	
 );
+
+create table [Empleados]
+(
+	[Id] int primary key references [Personas]([Id]), /*Heredando la misma clave primaria de Personas*/
+	[AnhosExperiencia] int not null,
+	[Salario] decimal not null
+);
+
+create table [Entrenadores]
+(
+	[Id] int primary key references [Empleados]([Id]), /*Heredando la misma clave primaria de PersonasEmpleados*/
+	[Especialidad] nvarchar(50) not null
+);
+
+create table [OtrosEmpleados]
+(
+	[id] int primary key references [Empleados]([Id]), /*Heredando la misma clave primaria de PersonasEmpleados*/
+	[Oficio] nvarchar(30) not null,
+	[Turno] nvarchar(6) not null
+);
+
+create table [Proveedores]
+(
+	[Id] int primary key identity(1,1),
+	[NombreEntidad] nvarchar(20) not null,
+	[ValorTotalVenta] decimal not null
+);
+
+create table [Instrumentos]
+(
+	[Id] int primary key identity(1,1),
+	[NombreInstrumento] nvarchar(20) not null,
+	[DescripcionGeneral] nvarchar(100),
+	[Estado] bit not null,
+	[Proveedor] int references [Proveedores]([Id]) 
+);
+
+create table [Equipamientos]
+(
+	[Id] int primary key references [Instrumentos]([Id]), /*Heredando la misma clave primaria de Instrumentos*/
+	[CantidadEquipamientos] int not null,
+	[Peso] decimal not null
+);
+
+create table [Maquinarias]
+(
+	[Id] int primary key references [Instrumentos]([Id]), /*Heredando la misma clave primaria de Instrumentos*/
+	[PesoMax] decimal not null,
+	[PesoMin] decimal not null,
+	[Piezas] int not null
+);
+
 
 create table [Membresias]
 (
@@ -37,7 +89,14 @@ create table [Suplementos]
 	[NombreSuplemento] nvarchar(20) not null,
 	[TipoSuplemento] nvarchar(10) not null,
 	[Valor] decimal not null,
-	[Cantidad] decimal not null
+	[Cantidad] decimal not null,
+	[Proveedor] int references [Proveedores]([Id])
+);
+
+create table [ClasesGrupales]
+(
+	[Id] int primary key identity(1,1),
+	[Duracion] decimal
 );
 
 	/*Tablas de relaciones muchos a muchos con Clientes*/
@@ -48,7 +107,7 @@ create table [ClientesMembresias]
 	[IdMembresias] int references [Membresias]([Id]) not null,
 	[FechaInicio] smalldatetime default getdate(),
 	[FechaFin] smalldatetime default getdate(),
-	[Id] int primary key (IdClientes, IdMembresias)
+	[Id] int primary key ([IdClientes], [IdMembresias])
 );
 
 /*Clientes-Suplementos*/
@@ -58,9 +117,24 @@ create table [ClientesSuplementos]
 	[IdSuplementos] int references [Suplementos]([Id]) not null,
 	[CantidadCompraSuplementos] int not null,
 	[ValorTotalCompra] decimal not null,
-	[Id] int primary key (IdClientes, IdSuplementos)
+	[Id] int primary key ([IdClientes], [IdSuplementos])
 );
 
+/*Clientes-ClasesGrupales*/
+create table [ClientesClasesGrupales]
+(
+	[IdClientes] int references [Clientes]([Id]) not null,
+	[IdClasesGrupales] int references [ClasesGrupales]([Id]) not null,
+	[Asistencias] int,
+	[Id] int primary key ([Idclientes], [IdClasesGrupales])
+);
 
-
+	/*Tabla de relación muchos a muchos con Entrenadores*/
+/*Entrenadores-ClasesGrupales*/
+create table [EntrenadoresClasesGrupales]
+(
+	[IdEntrenadores] int references [Entrenadores]([Id]) not null,
+	[IdClasesGrupales] int references [ClasesGrupales]([Id]) not null,
+	[Id] int primary key (IdEntrenadores, IdClasesGrupales)
+);
 
